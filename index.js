0de2eda5bc98e14a6f968a617c7e4294b8f41675
John Otander <johnotander@gmail.com>
2014-11-23 16:40:31 -0700

    Begin adding basic functionality, based off broccoli-jshint

---
+++ index.js
@@ -0,0 +1,135 @@
+// Adapted from https://github.com/rwjblue/broccoli-jshint/blob/master/index.js
+
+'use strict';
+
+var csslint = require('csslint').CSSLint;
+var Filter = require('broccoli-filter');
+var path = require('path');
+var chalk = require('chalk');
+var fs = require('fs');
+var findup = require('findup-sync');
+var mkdirp = require('mkdirp');
+
+CSSLinter.prototype = Object.create(Filter.prototype);
+CSSLinter.prototype.constructor = CSSLinter;
+
+function CSSLinter(inputTree, options) {
+  if (!(this instanceof CSSLinter)) {
+    return new CSSLinter(inputTree, options);
+  }
+
+  options = options || {};
+
+  this.inputTree = inputTree;
+  this.log       = true;
+
+  for (var key in options) {
+    if (options.hasOwnProperty(key)) {
+      this[key] = options[key]
+    }
+  }
+};
+
+CSSLinter.prototype.extensions = ['css'];
+
+CSSLinter.prototype.write = function (readTree, destDir) {
+  var self = this
+  self._errors = [];
+
+  return readTree(this.inputTree).then(function (srcDir) {
+    if (!self.csslintrc) {
+      var csslintrcPath = self.csslintrcPath || path.join(srcDir, self.csslintrcRoot || '');
+      self.csslintrc = self.getConfig(csslintrcPath);
+    }
+
+    return Filter.prototype.write.call(self, readTree, destDir)
+  })
+  .finally(function() {
+    if (self._errors.length > 0) {
+      console.log(self._errors);
+      var label = ' CSSLint Error' + (self._errors.length > 1 ? 's' : '')
+      console.log('\n' + self._errors.join('\n'));
+      console.log(chalk.yellow('===== ' + self._errors.length + label + '\n'));
+    }
+  });
+}
+
+CSSLinter.prototype.processString = function (content, relativePath) {
+  var report = csslint.verify(content, this.csslintrc);
+  var errors = this.processMessages(relativePath, report.messages);
+
+  console.log(report);
+
+  if (report.messages.length > 0) {
+    this.logError(errors);
+  }
+};
+
+CSSLinter.prototype.processMessages = function (file, messages) {
+  return messages.map(function() {
+    // ...
+  });
+}
+
+CSSLinter.prototype.logError = function(message, color) {
+  color = color || 'red';
+
+  this._errors.push(chalk[color](message) + "\n");
+};
+
+CSSLinter.prototype.getConfig = function(rootPath) {
+  if (!rootPath) { rootPath = process.cwd(); }
+
+  var ruleset = {};
+  var lintOptions = {};
+
+  var csslintrc = findup('.csslintrc', { cwd: rootPath, nocase: true });
+
+  if (csslintrc) {
+    var config = fs.readFileSync(csslintrc, { encoding: 'utf8' });
+
+    try {
+      lintOptions = JSON.parse(this.stripComments(config));
+    } catch (e) {
+      console.error(chalk.red('Error occured parsing .csslintrc.'));
+      console.error(e.stack);
+
+      return null;
+    }
+  }
+
+  // Build a list of all available rules
+  csslint.getRules().forEach(function(rule) {
+    ruleset[rule.id] = 1;
+  });
+
+  for (var rule in lintOptions) {
+    if (!lintOptions[rule]) {
+      // Remove rules that are turned off
+      delete ruleset[rule];
+    }
+    else {
+      ruleset[rule] = lintOptions[rule];
+    }
+  }
+
+  return ruleset;
+};
+
+CSSLinter.prototype.stripComments = function(string) {
+  string = string || "";
+
+  string = string.replace(/\/\*(?:(?!\*\/)[\s\S])*\*\//g, "");
+  string = string.replace(/\/\/[^\n\r]*/g, ""); // Everything after '//'
+
+  return string;
+};
+
+CSSLinter.prototype.escapeErrorString = function(string) {
+  string = string.replace(/\n/gi, "\\n");
+  string = string.replace(/'/gi, "\\'");
+
+  return string;
+};
+
+module.exports = CSSLinter;
--- test/test.js
+++ test/test.js
@@ -1,6 +1,50 @@
-var assert   = require('assert'),
-    fs       = require('fs');
+// Adapted from https://github.com/rwjblue/broccoli-jshint/blob/master/tests/index.js
+
+'use strict';
+
+var cssLint = require('..');
+var expect = require('expect.js');
+var fs = require('fs');
+var broccoli = require('broccoli');
+var root = process.cwd();
+
 
 describe('broccoli-csslint', function() {
-  it('will eventually do the things...');
+  var loggerOutput;
+  var builder;
+
+  function readFile(path) {
+    return fs.readFileSync(path, {encoding: 'utf8'});
+  }
+
+  function chdir(path) {
+    process.chdir(path);
+  }
+
+  beforeEach(function() {
+    chdir(root);
+
+    loggerOutput = [];
+  });
+
+  afterEach(function() {
+    if (builder) {
+      builder.cleanup();
+    }
+  });
+
+  it('uses the csslintrc as configuration for linting', function() {
+    var sourcePath = 'test/fixtures/valid-css-file';
+    chdir(sourcePath);
+
+    var tree = cssLint('.', {
+      logError: function(message) { loggerOutput.push(message) }
+    });
+
+    builder = new broccoli.Builder(tree);
+    return builder.build().then(function() {
+      console.log(loggerOutput);
+      expect(loggerOutput.join('\n')).to.not.match(/Missing semicolon./)
+    });
+  });
 });
